0cce4d7ebc0813759f665ddf2252ad1b87f7314b
metonator <metonator@gmail.com>
2018-10-03 17:26:33 +0200

    Update

--- GameLauncherUpdate/Form1.Designer.cs
+++ GameLauncherUpdate/Form1.Designer.cs
@@ -28,62 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.downloadProgress = new System.Windows.Forms.ProgressBar();
-            this.information = new System.Windows.Forms.Label();
-            this.animateMe = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.animateMe)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // downloadProgress
-            // 
-            this.downloadProgress.Location = new System.Drawing.Point(12, 233);
-            this.downloadProgress.Name = "downloadProgress";
-            this.downloadProgress.Size = new System.Drawing.Size(331, 10);
-            this.downloadProgress.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
-            this.downloadProgress.TabIndex = 0;
-            // 
-            // information
-            // 
-            this.information.AutoSize = true;
-            this.information.BackColor = System.Drawing.Color.Transparent;
-            this.information.ForeColor = System.Drawing.Color.Snow;
-            this.information.Location = new System.Drawing.Point(9, 214);
-            this.information.Name = "information";
-            this.information.Size = new System.Drawing.Size(131, 13);
-            this.information.TabIndex = 1;
-            this.information.Text = "Checking for latest update";
-            // 
-            // animateMe
-            // 
-            this.animateMe.BackColor = System.Drawing.Color.Transparent;
-            this.animateMe.Image = global::GameLauncherUpdate.Properties.Resources.icon_100;
-            this.animateMe.Location = new System.Drawing.Point(12, 12);
-            this.animateMe.Name = "animateMe";
-            this.animateMe.Size = new System.Drawing.Size(331, 187);
-            this.animateMe.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.animateMe.TabIndex = 2;
-            this.animateMe.TabStop = false;
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Black;
-            this.BackgroundImage = global::GameLauncherUpdate.Properties.Resources.luncher;
-            this.ClientSize = new System.Drawing.Size(355, 255);
-            this.Controls.Add(this.animateMe);
-            this.Controls.Add(this.information);
-            this.Controls.Add(this.downloadProgress);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "Form1";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Update";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.animateMe)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.downloadProgress = new System.Windows.Forms.ProgressBar();
+			this.information = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// downloadProgress
+			// 
+			this.downloadProgress.Location = new System.Drawing.Point(12, 31);
+			this.downloadProgress.Name = "downloadProgress";
+			this.downloadProgress.Size = new System.Drawing.Size(331, 10);
+			this.downloadProgress.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+			this.downloadProgress.TabIndex = 0;
+			// 
+			// information
+			// 
+			this.information.AutoSize = true;
+			this.information.BackColor = System.Drawing.Color.Transparent;
+			this.information.ForeColor = System.Drawing.Color.Snow;
+			this.information.Location = new System.Drawing.Point(9, 9);
+			this.information.Name = "information";
+			this.information.Size = new System.Drawing.Size(131, 13);
+			this.information.TabIndex = 1;
+			this.information.Text = "Checking for latest update";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Black;
+			this.BackgroundImage = global::GameLauncherUpdate.Properties.Resources.luncher;
+			this.ClientSize = new System.Drawing.Size(355, 53);
+			this.Controls.Add(this.information);
+			this.Controls.Add(this.downloadProgress);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Update";
+			this.TopMost = true;
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
@@ -91,7 +77,6 @@
 
         private System.Windows.Forms.ProgressBar downloadProgress;
         private System.Windows.Forms.Label information;
-        private System.Windows.Forms.PictureBox animateMe;
     }
 }
 
--- GameLauncherUpdate/Form1.cs
+++ GameLauncherUpdate/Form1.cs
@@ -1,5 +1,4 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +11,8 @@ using System.Text;
 using System.Windows.Forms;
 using System.IO.Compression;
 using System.Threading;
+
+using SimpleJSON;
 
 namespace GameLauncherUpdate {
     public partial class Form1 : Form {
@@ -40,34 +41,30 @@ namespace GameLauncherUpdate {
                 Process.GetProcessById(Convert.ToInt32(args[1])).Kill();
             }
 
-            Delay.WaitSeconds(1);
-
             if (File.Exists("GameLauncher.exe")) {
                 var versionInfo = FileVersionInfo.GetVersionInfo("GameLauncher.exe");
                 version = versionInfo.ProductVersion;
-                success("Found version " + version + ". Checking for update...");
             } else {
                 version = "0.0.0.0";
-                success("Failed to find GameLauncher.exe. Redownloading launcher.");
             }
 
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 
             var client = new WebClient();
-            Uri StringToUri = new Uri("http://launcher.soapboxrace.world/checkUpdate.php?version=" + version);
+            Uri StringToUri = new Uri("https://launchpad.soapboxrace.world/launcher/update?version=" + version);
             client.CancelAsync();
             client.DownloadStringAsync(StringToUri);
             client.DownloadStringCompleted += (sender2, e2) => {
                 try {
-                    CheckVersion json = JsonConvert.DeserializeObject<CheckVersion>(e2.Result);
+					JSONNode json = JSON.Parse(e2.Result);
 
-                    if(json.update.info != false) {
+					if (json["payload"]["update_exists"] != false) {
                         Thread thread = new Thread(() => {
                             WebClient client2 = new WebClient();
                             client2.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
                             client2.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
-                            client2.DownloadFileAsync(new Uri(json.update.download), tempNameZip);
-                        });
+                            client2.DownloadFileAsync(new Uri(json["payload"]["update"]["download_url"]), tempNameZip);
+						});
                         thread.Start();
                     } else {
                         Process.Start(@"GameLauncher.exe");
@@ -124,13 +121,13 @@ namespace GameLauncherUpdate {
 
                             Directory.CreateDirectory(folderName);
                         } else {
-                            if (fullName != "Newtonsoft.Json.dll" && fullName != "GameLauncherUpdate.exe" && fullName != "GameLauncherUpdate.pdb") {
+                            if (fullName != "GameLauncherUpdate.exe") {
                                 if (File.Exists(fullName)) {
                                     File.Delete(fullName);
                                 }
 
                                 information.Text = "Extracting: " + fullName;
-                                entry.ExtractToFile(Path.Combine(updatePath, fullName));
+								try { entry.ExtractToFile(Path.Combine(updatePath, fullName)); } catch { }
                                 Delay.WaitMSeconds(200);
                             }
                         }
@@ -146,62 +143,9 @@ namespace GameLauncherUpdate {
         }
 
         private void Form1_Load(object sender, EventArgs e) {
-            this.BeginInvoke((MethodInvoker)delegate {
-                Thread x = new Thread(animateMeFunc);
-                x.Start();
-
-                //animateMeFunc();
-                update();
-            });
-        }
-
-        public void animateMeFunc() {
-            while (op < 110) {
-
-                while (op > 40) {
-                    op = op - 10;
-                    Application.DoEvents();
-                    System.Threading.Thread.Sleep(100);
-
-                    if(op == 100) {
-                        animateMe.Image = Properties.Resources.icon_100;
-                    } else if (op == 90) {
-                        animateMe.Image = Properties.Resources.icon_90;
-                    } else if (op == 80) {
-                        animateMe.Image = Properties.Resources.icon_80;
-                    } else if (op == 70) {
-                        animateMe.Image = Properties.Resources.icon_70;
-                    } else if (op == 60) {
-                        animateMe.Image = Properties.Resources.icon_60;
-                    } else if (op == 50) {
-                        animateMe.Image = Properties.Resources.icon_50;
-                    } else {
-                        animateMe.Image = Properties.Resources.icon_40;
-                    }
-                }
-
-                while (op < 100) {
-                    op = op + 10;
-                    Application.DoEvents();
-                    System.Threading.Thread.Sleep(100);
-
-                    if (op == 100) {
-                        animateMe.Image = Properties.Resources.icon_100;
-                    } else if (op == 90) {
-                        animateMe.Image = Properties.Resources.icon_90;
-                    } else if (op == 80) {
-                        animateMe.Image = Properties.Resources.icon_80;
-                    } else if (op == 70) {
-                        animateMe.Image = Properties.Resources.icon_70;
-                    } else if (op == 60) {
-                        animateMe.Image = Properties.Resources.icon_60;
-                    } else if (op == 50)  {
-                        animateMe.Image = Properties.Resources.icon_50;
-                    } else {
-                        animateMe.Image = Properties.Resources.icon_40;
-                    }
-                };
-            }
-        }
+			this.BeginInvoke((MethodInvoker)delegate {
+				update();
+			});
+		}
     }
 }
